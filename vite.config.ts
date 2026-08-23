d6aad62730532be0fedd946c2886df07e84b094f
MaxExpert360 <maxexpert360@gmail.com>
2026-08-23 12:41:30 -0400

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/MaxExpert360mobile/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
